eecfb0020890441fbff65c8f49b492ebe1881124
Charlie <chp223@lehigh.edu>
2020-10-05 20:30:57 -0700

    Initial Commit

---
+++ workoutApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  workoutApp
+//
+//  Created by Charlie Perkins on 10/5/20.
+//  Copyright © 2020 Charlie Perkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
